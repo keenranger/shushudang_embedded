6211ef7a7575c3642ff05a52d562f1cff4a29d5c
HanKyeol Kyung <kghnkl0103@gmail.com>
2019-08-23 15:06:25 +0900

    upper arduino done

--- upper/upper.ino
+++ upper/upper.ino
@@ -8,40 +8,49 @@
 #define dryer1_power_pin 1
 #define dryer2_power_pin 2
 #define dryer3_power_pin 7
-#define bath_power_pin 8'
+#define bath_power_pin 8
 
 #define dryer1_intensity_pin 3
 #define dryer1_temperature_pin 6
 #define dryer2_intensity_pin 9
 #define dryer2_temperature_pin 10
 #define bath_intensity_pin 11
-
-int dryer1_state[3] = {0, 0, 0}; //power,intensity,temp
-int dryer2_state[3] = {0, 0, 0}; //power,intensity,temp
-int bath_state[3] = {0, 0, 0};
-char raspi_state[2] = {'f', 'f'}; //라즈베리파이 전원 기본상태 = f(꺼짐) //t=켜짐
-int raspi_pin[2] = {12, 13}; //라즈베리파이 핀 전원확인 핀
-int slave_addr[2] = {0x04, 0x05} //드라이/욕조 i2c 주소
+int dryer_on=0;
+int bath_on=0;
+int dryer_state[3][3] = {{0, 0, 0},{0, 0, 0}}; //power,intensity,temp
+int bath_state=0;
+int slave_addr[2] = {0x04, 0x05}; //드라이/욕조 i2c 주소
+int device_code=0; //1 for dryer, 2 for bath
 void setup() {
   Wire.begin();
-  pinMode(12, INPUT);
-  pinMode(13, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  for (int i = 0; i < 2; i++) { //라즈베리파이로부터 결제되었는지 확인하는 부분
-    if (digitalRead(raspi_pin[i]) == HIGH)
-      raspi_state[i] = 't';
-    else
-      raspi_state[i] = 'f';
-  }
-  for (int i = 0; i < 2; i++) {//결제여부를 밑의 아두이노에게 i2c로 보내는 부분
-    Wire.beginTransmission(slave_addr[i]);
-    Wire.write(raspi_state[i]);
-    Wire.endTransmission();
-  }
-  if (             d.;A[0] == 't' && 0] == 1) { //드라이어 결제되었고, 드라이어 on이면
+  Wire.requestFrom(slave_addr[0], 8);    // request 7 bytes from slave device 0x04
+  i2c_decoder(0);
+  Wire.requestFrom(slave_addr[1], 3);    // request 3 bytes from slave device 0x05
+  i2c_decoder(1);
+}
 
+void i2c_decoder(int i){
+
+
+  while (Wire.available()) { // slave may send less than requested
+    if (device_code==0){//첫 코드 읽기
+      device_code=Wire.read();
+    }
+    if (device_code==1){//코드가 드라이어 코드이면
+      dryer_on==Wire.read();//결제여부
+      for(int i=0; i<2;i++){//드라이어 1,2
+        for(int j=0; j<3;j++){//일시정지/강도/온도
+          dryer_state[i][j]=Wire.read();
+        }
+      }
+    }
+    else if(device_code==2){//코드가 배스 코드이면
+      bath_on=Wire.read();//일시정지 여부
+      bath_state=Wire.read();//세기
+    }
   }
 }
